bc5ef1c177c1ed2723a20d7ee70990155b852427
Daniel Monteiro <monteiroquiet@gmail.com>
2024-07-20 15:16:04 +0100

    fix: make defending meaning no damage is caused, but also doubles the defense

--- common/src/BattleScreen.c
+++ common/src/BattleScreen.c
@@ -415,38 +415,43 @@ enum EGameMenuState BattleScreen_tickCallback(enum ECommand cmd, void *data) {
                     }
 
                     for (c = 0; c < (TOTAL_CHARACTERS_IN_PARTY); ++c) {
-                        
-                        
-                        
                         if (battleActions[c] == kAttack) {
                             battleTargets[c] = TOTAL_CHARACTERS_IN_PARTY + (rand() % (aliveMonsters));
                         }
                     }
 
                     for (c = 0; c < TOTAL_CHARACTERS_IN_PARTY; ++c) {
-                        int monsterTargetted = battleTargets[c];
-                        int attack = (rand() % party[c].attack);
-                        int agil1 = party[c].agility;
-                        int agil2 = monsterArchetypes[monsterType[monsterTargetted]].agility;
-                        int diffAgility = 1 + abs( agil1 - agil2);
-                        int roll = rand();
-                        int hit = roll % diffAgility;
-                        int defense = monsterArchetypes[monsterType[monsterTargetted]].defense;
-                        int calc = attack - ((hit == 0) ? 0 : defense);
-                        battleDamages[c] = max(0, calc);
+                        if (battleActions[c] == kAttack) {
+                            int monsterTargetted = battleTargets[c];
+                            int attack = (rand() % party[c].attack);
+                            int agil1 = party[c].agility;
+                            int agil2 = monsterArchetypes[monsterType[monsterTargetted]].agility;
+                            int diffAgility = 1 + abs( agil1 - agil2);
+                            int roll = rand();
+                            int hit = roll % diffAgility;
+                            int defense = ((battleActions[c + TOTAL_CHARACTERS_IN_PARTY] == kDefend) ? 2 : 1) *  monsterArchetypes[monsterType[monsterTargetted]].defense;
+                            int calc = attack - ((hit == 0) ? 0 : defense);
+                            battleDamages[c] = max(0, calc);
+                        } else {
+                            battleDamages[c] = 0;
+                        }
                     }
 
                     for (c = 0; c < TOTAL_MONSTER_COUNT; ++c) {
-                        int heroTargetted = battleTargets[c + TOTAL_CHARACTERS_IN_PARTY];
-                        int attack = (rand() % monsterArchetypes[monsterType[c]].attack);
-                        int agil1 = party[heroTargetted].agility;
-                        int agil2 = monsterArchetypes[monsterType[c]].agility;
-                        int diffAgility = 1 + abs( agil1 - agil2 );
-                        int roll = rand();
-                        int hit = roll % diffAgility;
-                        int defense = party[heroTargetted].defense;
-                        int calc = attack - ((hit == 0) ? 0 : defense);
-                        battleDamages[c + TOTAL_CHARACTERS_IN_PARTY] = max(0, calc);
+                        if (battleActions[c + TOTAL_CHARACTERS_IN_PARTY] == kAttack) {
+                            int heroTargetted = battleTargets[c + TOTAL_CHARACTERS_IN_PARTY];
+                            int attack = (rand() % monsterArchetypes[monsterType[c]].attack);
+                            int agil1 = party[heroTargetted].agility;
+                            int agil2 = monsterArchetypes[monsterType[c]].agility;
+                            int diffAgility = 1 + abs( agil1 - agil2 );
+                            int roll = rand();
+                            int hit = roll % diffAgility;
+                            int defense = ((battleActions[c] == kDefend) ? 2 : 1) *  party[heroTargetted].defense;
+                            int calc = attack - ((hit == 0) ? 0 : defense);
+                            battleDamages[c + TOTAL_CHARACTERS_IN_PARTY] = max(0, calc);
+                        } else {
+                            battleDamages[c + TOTAL_CHARACTERS_IN_PARTY] = 0;
+                        }
                     }
 
                     animationTimer = kBattleAnimationInterval;
