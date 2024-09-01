b04068d9491f3c35b0d84f11a072deb41b6369a9
Daniel Monteiro <monteiroquiet@gmail.com>
2024-09-01 15:28:50 +0100

    fix: disable fullscreen on Android (MX), since it cause some weirdness
    And is deprecated anyway

--- mx_frontend/NDK-version/app/src/main/java/pt/b13h/spacetrashmanblues/MainActivity.kt
+++ mx_frontend/NDK-version/app/src/main/java/pt/b13h/spacetrashmanblues/MainActivity.kt
@@ -63,13 +63,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchList
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (Build.VERSION.SDK_INT >= 29) {
-            window.decorView.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        } else {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        }
-
         if  ( (application as SubMareImperiumApplication).mayEnableSound() ) {
             initAudio()
         } else {
